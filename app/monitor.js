f86a02717cf977d8b2cf00a9884edca0caa6901a
Carnevalli <verdi@hotmail.com.br>
2019-04-02 12:44:40 -0300

    monitor

--- app/monitor.js
+++ app/monitor.js
@@ -5,7 +5,7 @@ exports.characterInfo = async page => {
                 var inventory = `${character.items.filter(item => !!item).length}/${character.items.length}`;
                 var xp = `${((character.xp * 100) / character.max_xp).toFixed(2)}%`;
                 var target = Object.values(entities).filter(ent => ent.target == character.name)[0];
-                window.nb_logInfo(`Name: ${character.name} / Inventory: ${inventory} / XP: ${xp} / Gold: ${character.gold} / Map: ${character.map} / Target: ${target ? target.name : "none"}`);
+                window.nb_logInfo(`Name: ${character.name} / Inventory: ${inventory} / XP: ${xp} / Gold: ${character.gold} / Map: ${character.map} / Target: ${target ? target.name : "none"} / ${character.rip ? "Dead" : "Alive"}`);
             }
         }, 1000 * 10)
     );
