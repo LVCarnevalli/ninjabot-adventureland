eb1fe18a514f25d3dedba9866f378cecd426ac31
Carnevalli <verdi@hotmail.com.br>
2019-04-05 00:13:41 -0300

    monitor

--- app/monitor.js
+++ app/monitor.js
@@ -14,7 +14,7 @@ exports.characterInfo = async page => {
 exports.isDead = async page => {
     setInterval(async () => {
         let rip = await page.evaluate(() => character && character.rip);
-        if (rip) {
+        if (rip && character) {
             page.evaluate(() => {
                 setTimeout(function() {
                     window.nb_logInfo(`Warning: ${character.name} you DEAD!`);
@@ -27,7 +27,7 @@ exports.isDead = async page => {
 exports.runCode = async page => {
     setInterval(() => {
         page.evaluate(() => {
-            if (actual_code && code_run) {
+            if (actual_code && code_run && character) {
                 window.nb_logInfo(`${character.name} running code...`);
             } else {
                 window.nb_logInfo(`Warning: ${character.name} stopped code!`);
