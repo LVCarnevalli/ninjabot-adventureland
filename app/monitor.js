a7743c0052903507a69769bab8e68f4a3f8dc453
Carnevalli <verdi@hotmail.com.br>
2019-04-05 00:17:35 -0300

    monitor

--- app/monitor.js
+++ app/monitor.js
@@ -11,26 +11,15 @@ exports.characterInfo = async page => {
     }, 1000 * 10);
 };
 
-exports.isDead = async page => {
-    setInterval(async () => {
-        let rip = await page.evaluate(() => character && character.rip);
-        if (rip && character) {
-            page.evaluate(() => {
-                setTimeout(function() {
-                    window.nb_logInfo(`Warning: ${character.name} you DEAD!`);
-                }, 1000 * 12);
-            });
-        }
-    }, 1000 * 10);
-};
-
 exports.runCode = async page => {
     setInterval(() => {
         page.evaluate(() => {
-            if (actual_code && code_run && character) {
-                window.nb_logInfo(`${character.name} running code...`);
-            } else {
-                window.nb_logInfo(`Warning: ${character.name} stopped code!`);
+            if (character) {
+                if (actual_code && code_run) {
+                    window.nb_logInfo(`${character.name} running code...`);
+                } else {
+                    window.nb_logInfo(`Warning: ${character.name} stopped code!`);
+                }
             }
         });
     }, 1000 * 60);
